47e17eb3080fa3d28f02a5bd9093f49a133b132f
Georgy Martirosov <ya.goshka@gmail.com>
2012-03-12 15:26:02 +0400

    WIP

--- src/AY/GeneralBundle/Resources/public/js/common.js
+++ src/AY/GeneralBundle/Resources/public/js/common.js
@@ -44,4 +44,78 @@ function undef(o){
 		window.$.predefine = PredefinedText;
 	}
 	
-})()
+})();
+
+$.predefine('.predefined-text-control');
+
+(function(){
+	var c = function(container){
+		var cc = null,
+			that = this;
+		
+		this.container = cc = $(container);
+		
+		this.input = {
+			code: cc.find('[data-type="code"]'),
+			region: cc.find('[data-type="region"]')
+		}
+		
+		function handleInput(e){
+			that.validate.call(that, e);
+		}
+		
+		$(this.input.code).keyup(handleInput);
+		$(this.input.code).keyup(handleInput);			
+	}
+	
+	c.prototype = {
+		validator: {
+			code: function( str, char ){
+				var sl = str.length,
+					_rc = 'АВЕКМНОРСТУХ',
+					_r = '',
+					char = char || null;
+					
+				function validateChar(c){
+					if(sl > 1 && sl < 4){
+						_r = '\d';
+					}
+					
+					if(sl > 4 || sl == 1){
+						_r = '[' + _rc + ']';
+					}
+					
+					return new RegExp( _r ).test( с ) || false;;
+				}
+				
+				if(char){
+					return validateChar(char);
+				}
+				
+			},
+			
+			region: function( s ){
+				var _r = '\\d';
+
+				if( s.length > 1 )
+					_r += '{' + s.length + '}';
+
+				return new RegExp( _r ).test( s ) || false;
+			}
+		},
+		
+		validate: function( e ){
+			var t = $(e.target),
+				type = t.attr('data-type');
+			
+			var valid = this.validator[type](t.val(), String.fromCharCode(e.keyCode));
+			if(!valid){
+				return false;
+			}
+		}
+	};
+	
+	window.CarNumber = c;
+})();
+
+
